2225bff61bc9179bdb8262dd9b410e1b70f7baff
RameshTechmileage <ramesh@techmileage.com>
2016-09-15 09:16:00 -0700

    Adding log4j properties

--- src/main/java/com/amex/itag/util/DuplicateParameters.java
+++ src/main/java/com/amex/itag/util/DuplicateParameters.java
@@ -6,4 +6,9 @@ import org.springframework.http.HttpStatus;
 @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Duplicate Parameters found") 
 public class DuplicateParameters extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
